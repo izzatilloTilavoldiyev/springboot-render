fb8ee44e0eb4e7359863f7c54460e51966ff2beb
izzatillotilavoldiyev5 <izzatillotilavoldiyev5@gmail.com>
2023-10-05 16:35:25 +0500

    spring boot application committed

---
+++ src/main/java/uz/pdp/springbootrender/SpringbootRenderApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.springbootrender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRenderApplication.class, args);
+    }
+
+}
